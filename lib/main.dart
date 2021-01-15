ec283c4393b401caef4c49863a199de48f550591
Marlon Lamartine <marlon.lamartine@gmail.com>
2021-01-15 18:25:36 -0300

    Commit de config com firebase, custom drawer e custom tile

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual_2_0/screens/base/base_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Loja Online Teste',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BaseScreen(),
+    );
+  }
+}
+
