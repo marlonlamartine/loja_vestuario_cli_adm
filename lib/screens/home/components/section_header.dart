73016efa9847fb74f4ba725f02263d47822ae9e1
Marlon Lamartine <marlon.lamartine@gmail.com>
2021-07-27 02:16:37 -0300

    editando título/removendo seção

--- lib/screens/home/components/section_header.dart
+++ lib/screens/home/components/section_header.dart
@@ -1,25 +1,61 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_2_0/common/custom_icon_button.dart';
+import 'package:loja_virtual_2_0/models/home_manager.dart';
 import 'package:loja_virtual_2_0/models/section.dart';
+import 'package:provider/provider.dart';
 
 class SectionHeader extends StatelessWidget {
 
-  SectionHeader(this.section);
+  const SectionHeader(this.section);
 
   final Section section;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Text(
+
+    final homeManager = context.watch<HomeManager>();
+
+    if(homeManager.editing){
+      return Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              initialValue: section.name,
+              decoration: const InputDecoration(
+                hintText: 'Título',
+                isDense: true,
+                border: InputBorder.none,
+              ),
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+                fontSize: 18
+              ),
+              onChanged: (text) => section.name = text,
+            ),
+          ),
+          CustomIconButton(
+            iconData: Icons.remove,
+            color: Colors.white,
+            onTap: (){
+              homeManager.removeSection(section);
+            },
+          ),
+        ],
+      );
+    }
+    else{
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Text(
           section.name ?? "",
-        style: const TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.w800,
-          fontSize: 18
+          style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w800,
+              fontSize: 18
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
