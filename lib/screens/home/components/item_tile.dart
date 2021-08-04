db685b1166506e6eb41332b1626f6ec5ca2b57b3
Marlon Lamartine <marlon.lamartine@gmail.com>
2021-08-04 15:20:47 -0300

    vinculando/desvinculando produto

--- lib/screens/home/components/item_tile.dart
+++ lib/screens/home/components/item_tile.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual_2_0/models/home_manager.dart';
+import 'package:loja_virtual_2_0/models/product.dart';
 import 'package:loja_virtual_2_0/models/product_manager.dart';
 import 'package:loja_virtual_2_0/models/section.dart';
 import 'package:loja_virtual_2_0/models/section_item.dart';
 import 'package:provider/provider.dart';
 import 'package:transparent_image/transparent_image.dart';
-import 'package:provider/provider.dart';
+
 
 class ItemTile extends StatelessWidget {
 
@@ -33,8 +34,18 @@ class ItemTile extends StatelessWidget {
         showDialog(
           context: context,
           builder: (_){
+            final product = context.read<ProductManager>()
+                .findProductById(item.product);
             return AlertDialog(
               title: const Text('Editar Item'),
+              content: product != null
+              ? ListTile(
+                contentPadding: EdgeInsets.zero,
+                leading: Image.network(product.images.first),
+                title: Text(product.name),
+                subtitle: Text('R\$ ${product.basePrice.toStringAsFixed(2)}'),
+              )
+              : null,
               actions: [
                 ElevatedButton(
                     onPressed: (){
@@ -43,6 +54,24 @@ class ItemTile extends StatelessWidget {
                     },
                     style: ElevatedButton.styleFrom(primary: Colors.red),
                     child: const Text('Excluir'),
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      if(product != null){
+                        item.product = null;
+                      }
+                      else{
+                        final Product product = await Navigator.of(context).
+                              pushNamed('/select_product') as Product;
+                        item.product = product?.id;
+                      }
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      product != null
+                          ? 'Desvincular'
+                          : 'Vincular'
+                    )
                 )
               ],
             );
