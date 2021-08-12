043f88837ed92eb30e0e6d9afc11b5ba6b59fd24
Marlon Lamartine <marlon.lamartine@gmail.com>
2021-08-12 02:42:18 -0300

    ordenando seções e exibindo o carregamento

--- lib/models/section.dart
+++ lib/models/section.dart
@@ -49,11 +49,12 @@ class Section extends ChangeNotifier{
     notifyListeners();
   }
 
-  Future<void> save() async
+  Future<void> save(int pos) async
   {
     final Map<String, dynamic> data = {
       'name': name,
-      'type': type
+      'type': type,
+      'pos': pos,
     };
 
     if(id == null){
@@ -92,6 +93,19 @@ class Section extends ChangeNotifier{
 
     await firestoreRef.updateData(itemsData);
 
+  }
+
+  Future<void> delete() async{
+    await firestoreRef.delete();
+    for(final item in items){
+      try {
+        final ref = await storage.getReferenceFromUrl(
+            item.image as String
+        );
+        await ref.delete();
+        //ignore: empty_catches
+      } catch(e){}
+    }
   }
 
   bool valid(){
